e34002e416c8d2144deccf9dede9c8d6248e596e
david p <davidperrai@gmail.com>
2016-12-12 12:36:31 +0100

    correction

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
 
-  reponse.sendFile(__dirname+'/views/index.html');
+  response.sendFile(__dirname+'/views/index.html');
 
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
